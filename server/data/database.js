d818d26bffc46a63ee61b5fd8967f9c2d7139b78
Kasey Munetake <kaseymunetake@gmail.com>
2017-01-31 11:11:07 -0800

    Added database file with viewer and todos

---
+++ server/data/database.js
@@ -0,0 +1,70 @@
+/* @flow */
+
+// mock data
+export class Todo {
+  id: string;
+  content: string;
+}
+export class User {
+  id: string;
+}
+
+// main viewer (default user)
+const VIEWER = 'viewer';
+
+const viewer = new User();
+viewer.id = VIEWER;
+
+// storage for all users
+const usersById = {
+  [VIEWER]: viewer,
+};
+
+// storage for all todos
+const todosById = {};
+
+// todos for a user
+const todoIdsByUser = {
+  [VIEWER]: [],
+};
+
+// global todo id
+let nextTodoId = 0;
+
+export function addTodo(content: string) {
+  const todo = new Todo();
+  todo.id = `${nextTodoId++}`;
+  todo.content = content;
+  todosById[todo.id] = todo;
+  todoIdsByUser[VIEWER].push(todo.id);
+  return todo.id;
+}
+
+export function getTodo(id: string) {
+  return todosById[id];
+}
+
+export function getUser(id: string) {
+  return usersById[id];
+}
+
+export function getViewer() {
+  return getUser(VIEWER);
+}
+
+export function getTodos() {
+  return todoIdsByUser[VIEWER].map(id => todosById[id]);
+}
+
+export function removeTodo(id: string) {
+  const index = todoIdsByUser[VIEWER].indexOf(id);
+  if (index !== -1) {
+    todoIdsByUser[VIEWER].splice(index, 1);
+  }
+  delete todosById[id];
+}
+
+export function updateTodo(id: string, content: string) {
+  const todo = getTodo(id);
+  todo.content = content;
+}
