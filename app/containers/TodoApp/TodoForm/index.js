ab62e3ed81ff9050c9de07deb1db260e99380a96
munetake <kaseymunetake@gmail.com>
2017-02-01 15:28:44 -0800

    Updated TodoForm to handle undefined addTodo prop

--- app/containers/TodoApp/TodoForm/index.js
+++ app/containers/TodoApp/TodoForm/index.js
@@ -3,7 +3,7 @@
 import React from 'react';
 
 type Props = {
-  addTodo: () => void, // callback to create new todo in parent state
+  addTodo: ?Function, // callback to create new todo in parent state
 };
 
 type State = {
@@ -40,8 +40,14 @@ export default class TodoForm extends React.Component {
   }
 
   handleSubmit(event) {
+    const {
+      addTodo,
+    } = this.props;
+
+    if (addTodo) {
+      addTodo(this.state.content);
+    }
     event.preventDefault();
-    this.props.addTodo(this.state.content);
     this.setState({
       content: '',
     });
