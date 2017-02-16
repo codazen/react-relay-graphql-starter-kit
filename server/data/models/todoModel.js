d637eaa2579cefa699b0aca5b51e3f63b57fb9df
Kasey Munetake <kaseymunetake@gmail.com>
2017-02-15 16:41:40 -0800

    Added add, update, delete functions for todo

--- server/data/models/todoModel.js
+++ server/data/models/todoModel.js
@@ -8,3 +8,34 @@ const TodoSchema = new mongoose.Schema({
 
 const Todo = mongoose.model('Todo', TodoSchema);
 exports.TodoModel = Todo;
+
+exports.addTodo = (content: string) => {
+  const newTodo = new Todo({ content });
+  return new Promise((resolve, reject) => {
+    newTodo.save((err, todo) => {
+      err ? reject(err) : resolve(todo); // eslint-disable-line no-unused-expressions
+    });
+  });
+};
+
+exports.updateTodo = (_id: string, content: string) =>
+  new Promise((resolve, reject) => {
+    Todo.findByIdAndUpdate(
+      { _id: mongoose.Types.ObjectId(_id) },
+      { content },
+      { new: true },
+      (err, todo) => {
+        err ? reject(err) : resolve(todo); // eslint-disable-line no-unused-expressions
+      },
+    );
+  });
+
+exports.deleteTodo = (_id: string) =>
+  new Promise((resolve, reject) => {
+    Todo.findByIdAndRemove(
+      { _id: mongoose.Types.ObjectId(_id) },
+      (err, todo) => {
+        err ? reject(err) : resolve(todo); // eslint-disable-line no-unused-expressions
+      },
+    );
+  });
